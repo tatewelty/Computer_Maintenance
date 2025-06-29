92b5fe422f9d269838100c8c67169197d68ba8dc
tatewelty <87451665+tatewelty@users.noreply.github.com>
2025-06-29 13:07:48 -0500

    Added Files to Repo
    Added the .bat files

---
+++ Computer_Maintenance_Setup.bat
@@ -0,0 +1,13 @@
+@echo off
+title Computer Maintenance One Time Setup
+
+echo Please select file cleanup options you want in the window that opens
+echo This configuration will be saved and used in future cleanups
+cleanmgr /sageset:1
+echo Configuration saved!
+
+echo Installing Windows Update Scripts
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Install-PackageProvider -Name NuGet -Force; Install-Module PSWindowsUpdate -force"
+echo Successfully Installed Windows Update Scripts
+
+pause
